284a6db72992c3ad9619db919be599cce9f14feb
MrAzuka <johntwice8@gmail.com>
2025-01-03 01:49:13 +0100

    initial setup and migration files

---
+++ database/migrations/2025_01_02_233148_create_reviews_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('reviewer_id'); 
+            $table->unsignedBigInteger('reviewee_id');
+            $table->foreignId('ad_id')->constrained()->onDelete('cascade'); 
+            $table->longText('comment');
+            $table->integer('rating');
+            $table->timestamps();
+
+            // Define foreign key constraints
+            $table->foreign('reviewer_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade'); 
+
+            $table->foreign('reviewee_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
